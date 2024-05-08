258ce6675915240b2f9a11a55660f429d1350ca6
loi1220 <loi1220.dev@outlook.com>
2024-05-09 00:24:26 +0900

    vercel17

--- pages/[lang]/g/[slug].js
+++ pages/[lang]/g/[slug].js
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 export default function Game({ pageData, allGamesData }) {
     // const gameData = pageData.game;
-    console.log(pageData.game.players);
+    console.log(pageData);
     const [favs, setFavs] = useState([]);
     useEffect(() => {
         const storedData = localStorage.getItem('fav');
@@ -34,7 +34,6 @@ export default function Game({ pageData, allGamesData }) {
             <div className="grid grid-cols-4 gap-10">
                 <div className="col-span-3 text-white">
                     <div className="flex justify-between pb-4">
-                        {JSON.stringify(pageData.game)}
                         {/* <div className="justify-start text-4xl">{pageData}</div> */}
                         {/* <div className="justify-end">
                             <button
