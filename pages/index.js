f211ec607d377ae088ba87da78920641a5a80369
loi1220 <loi1220.dev@outlook.com>
2024-05-08 23:03:44 +0900

    vercel1

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ allGamesData, pageData }) {
     <Layout home pageData={pageData} allGamesData={allGamesData}>
       {gamesData.map(({ gameSlug }) => (
         <Link href={`/en/g/${gameSlug}`} className="" key={gameSlug}>
-          <Image className='rounded-lg' width="100%" height="100%" alt="" src={`https://webgamer.io/games/${gameSlug}/${gameSlug}.240x.85pc.webp`} loading="eager"/>
+          <img className='rounded-lg' width="100%" height="100%" alt="" src={`https://webgamer.io/games/${gameSlug}/${gameSlug}.240x.85pc.webp`} loading="eager"/>
         </Link>
       ))}
     </Layout>
