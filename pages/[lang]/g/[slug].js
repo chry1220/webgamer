0fd6fa070281cd27e3eb89c660001894dbf97e91
loi1220 <loi1220.dev@outlook.com>
2024-05-09 00:13:09 +0900

    vercel12

--- pages/[lang]/g/[slug].js
+++ pages/[lang]/g/[slug].js
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 export default function Game({ pageData, allGamesData }) {
     const gameData = pageData.game;
-    console.log(gameData);
+    console.log(gameData.name);
     const [favs, setFavs] = useState([]);
     useEffect(() => {
         const storedData = localStorage.getItem('fav');
